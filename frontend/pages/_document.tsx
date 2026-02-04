3f72a93b39d0b309adbaa59ec691d4ac82c86572
saipoo <poornaseshaseyanaraja@gmail.com>
2026-02-04 20:44:47 +0530

    Add Google site verification meta tag, sitemap.xml and robots.txt for SEO

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
+        <meta name="google-site-verification" content="cTHHiPfv84dGvgZZXBf0KYMYZOpc7dzlc3dweQ5LFz8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </Head>
